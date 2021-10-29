f14dba7ac5bf64dc6f03028ddf90d4b47eff1f01
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-29 21:10:44 +0300

    adds the API key in class config

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 #initializing application
-app= Flask(__name__)
-
+app= Flask(__name__,instance_relative_config=True)
+#Setting up Configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 from app import views
---
+++ app/config.py
@@ -0,0 +1,6 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q=Apple&from=2021-10-29&sortBy=popularity&apiKey=API_KEY'
+    
