f7c0230b59c6313eb9514399806a1d10f5326210
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-30 21:59:20 +0300

    adds class arguments

--- app/models/newssource.py
+++ app/models/newssource.py
@@ -4,3 +4,10 @@ class Source:
     Source class to define Movie Objects
     '''
     """
+def __init__(self,id,name,description,url,category):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+       
