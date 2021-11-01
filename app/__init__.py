f78d8ebadffba27e01a620f8ebb5ecf00dc6d8b0
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-01 12:30:46 +0300

    updates the init .py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap 
 #initializing application
 app= Flask(__name__,instance_relative_config=True)
 #Setting up Configuration
 app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+
 bootstrap = Bootstrap(app)
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
 
 from app import views
 from app import error
