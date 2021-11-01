046e957cf10ba986cc1b1015da8b6d4789466b90
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-01 12:25:56 +0300

    updates the config.py file

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+    
