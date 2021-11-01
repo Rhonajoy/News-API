a61c4b10a42ec08922e852f3002c1a59678deb95
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-02 02:25:38 +0300

    adds the tests article

--- tests/article_test.py
+++ tests/article_test.py
@@ -14,7 +14,7 @@ class SourceTest(unittest.TestCase):
         Set up method that will run before every Test
         '''
         self.new_article = Article('abc-news','ABC News','our trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.','https://abcnews.go.com/','general')
-         
+        #  ','CNN','skjsdjfkd.jpg','killer cat','killer','www.cnn.com','12/12/15','name
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Article))
--- tests/source_test.py
+++ tests/source_test.py
@@ -1,4 +1,4 @@
- import unittest
+import unittest
 class Source:
     """
      '''
@@ -29,5 +29,14 @@ class SourceTest(unittest.TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
+
+    def test_to_check_instance_variables(self):
+
+        self.assertEquals(self.new_source.id,'abc-news')
+        self.assertEquals(self.new_source.name,'ABC News')
+        self.assertEquals(self.new_source.description,'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.')
+        self.assertEquals(self.new_source.url,'https://abcnews.go.com')
+        self.assertEquals(self.new_source.category,'general')
+        
 
 
